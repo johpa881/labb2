0ff606122d67168b8d4b814e1e1098c0cc23c104
johpa <johpa881@student.liu.se>
2023-02-21 16:06:52 +0100

    11th commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hej igen' });
+  res.render('index', { title: 'Hej igen igen igen igen' });
 });
 
 module.exports = router;
